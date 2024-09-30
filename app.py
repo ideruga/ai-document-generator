5510b7fd46389905eb72bc5cb35fa360a8e7ed89
Igor Deruga <i.deruga@gmail.com>
2024-09-30 10:03:40 -0400

    Change cookie-based session to server-side file-based session; Use LangChain chaining to generate summaries for each chapter; Create a github action to generate a dockerfile and store it in the github container registry.

--- app.py
+++ app.py
@@ -2,8 +2,11 @@ import os
 
 from dotenv import load_dotenv
 from flask import Flask, render_template, request, session, jsonify
+from flask_session import Session
 from langchain.llms import OpenAI
+from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnablePassthrough, RunnableLambda
 
 load_dotenv()
 
@@ -15,17 +18,23 @@ if openai_api_key is None:
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
+
 llm = OpenAI(openai_api_key=openai_api_key)
 
 from datetime import datetime
+
 
 @app.context_processor
 def inject_current_year():
     return {'current_year': datetime.now().year}
 
+
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
 
 @app.route('/generate_toc', methods=['POST'])
 def generate_toc():
@@ -49,24 +58,60 @@ def generate_toc():
 
     return jsonify({'titles': titles})
 
+
+def prepare_inputs(output_ch1):
+    print("PREPARING INPUTS!!!!!")
+    print(output_ch1)
+    return {
+        "chapter_name": "Chapter 2: Happy Ending",
+        "previous_chapters_summary": "Prince is a handsome guy"  # Pass the summary from Chapter 1
+    }
+
+
 @app.route('/generate_all_chapters', methods=['POST'])
 def generate_all_chapters():
     chapters = session.get('chapters', [])
 
+    first_chapter_prompt = PromptTemplate(
+        input_variables=["book_title", "chapter_name"],
+        template="Generate a book chapter based on the book's title {book_title} and chapter's header: {chapter_name}. Do not include the chapter header, only the text."
+    )
+    other_chapter_prompt = PromptTemplate(
+        input_variables=["book_title", "chapter_name", "summary"],
+        template="Generate a book chapter based on the book's title {book_title} and chapter's header: {chapter_name}. "
+                 "Do not include the chapter header, only the text. "
+                 "For the information purposes only, here's the summary of the previous chapter, do not include this summary in the current chapter: {summary}."
+    )
+    summary_prompt = PromptTemplate(
+        input_variables=["document"],
+        template="Generate a short document summary for the following text: {document}."
+    )
+
+    latest_summary = None
     for index, chapter in enumerate(chapters):
-        chapter_prompt = PromptTemplate(
-            input_variables=["chapter_name"],
-            template="Generate a book chapter based on its header: {chapter_name}. Do not include the chapter header, only the text."
-        )
-        chapter_chain = chapter_prompt | llm
 
-        content = chapter_chain.invoke({"chapter_name": chapter})
-        chapters[index]['content'] = content
+        prompt = first_chapter_prompt if index == 0 else other_chapter_prompt
+        chapter_chain = prompt | llm | StrOutputParser()
+        summary_chain = summary_prompt | llm | StrOutputParser()
 
-    session['chapters'] = chapters
+        aggregate_chain = chapter_chain | {
+            "document": RunnablePassthrough(),
+            "summary": summary_chain
+        }
 
-    # Return the generated content
+        content = aggregate_chain.invoke({"book_title": session['description'],
+                                          "chapter_name": chapter,
+                                          "summary": latest_summary})
+        print("Aggregated content:")
+        print(content)
+        chapters[index]['content'] = content['document']
+        chapters[index]['summary'] = content['summary']
+        latest_summary = content['summary']
+
+        session['chapters'] = chapters
+
     return jsonify({'chapters': chapters})
+
 
 @app.route('/update_chapter_title', methods=['POST'])
 def update_chapter_title():
@@ -76,6 +121,7 @@ def update_chapter_title():
     chapters[chapter_index]['title'] = new_title
     session['chapters'] = chapters
     return jsonify({'success': True})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
